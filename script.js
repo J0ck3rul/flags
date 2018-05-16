5e0e892b59705f71af9896d9894c4bc995d19ad5
J0ck3rul <33978520+J0ck3rul@users.noreply.github.com>
2018-05-16 13:51:57 +0300

    still working

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-document.getElementById("romania").onmouseover = function() {mouseOver("romania")}
-document.getElementById("greece").onmouseover = function() {mouseOver("greece")}
-document.getElementById("india").onmouseover = function() {mouseOver("india")}
-document.getElementById("spain").onmouseover = function() {mouseOver("spain")}
+var maxPhoto = 4;
+window.onload =  SetSlide(1);
+var photo=1;
 
-document.getElementById("romania").onmouseout = function() {mouseOut("romania")}
-document.getElementById("greece").onmouseout = function() {mouseOut("greece")}
-document.getElementById("india").onmouseout = function() {mouseOut("india")}
-document.getElementById("spain").onmouseout = function() {mouseOut("spain")}
+function pressKey(event)
+{
+    console.log(event);
+}
 
 function mouseOver(string)
 {
@@ -15,15 +14,51 @@ function mouseOver(string)
     q= document.createElement("span");
     q.setAttribute("class","middle");
     q.setAttribute("id","ma-sa")
- //   q.setAtribute("class", "middle")
+   // q.setAtribute("class", "middle")
     q.innerHTML = string;
     q.style.zindex = '1';
-    document.getElementById(string+"1").appendChild(q);
+     document.getElementById("inde"+photo).appendChild(q);
 }
 function mouseOut(string)
 {
     console.log(string);
-   document.getElementById("ma-sa").remove();
+    document.getElementById("ma-sa").remove();
   
     document.getElementById(string).style.opacity = '1  ';
+}
+
+function plusImg(much)
+{   
+   
+    if(much===1)
+    photo-=1;
+    if(much===2)
+    photo+=1;
+   // console.log(photo);
+    if(photo===(maxPhoto+1))
+    photo=1;
+    if(photo===0)
+    photo=maxPhoto;
+  //  photo = photo + much;
+ 
+    console.log(photo);
+    SetSlide(photo);
+}
+
+function SetSlide(index)
+{  console.log(index);
+     photo = index;
+    document.getElementById("index" + index).style.display="block";
+
+   // q.style.display = "block";
+    let i;
+    for(i=1;i<=maxPhoto;i++)
+    {
+        if(i!=index)
+        {
+            document.getElementById("index"+i).style.display = "none";
+        }
+    }
+
+
 }
